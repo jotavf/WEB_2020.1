23a9856169612991a2a5cdf230d2850e343526f0
Joao Victor Falcao <jotavfalcao@gmail.com>
2020-07-24 01:32:07 -0300

    questao3 pegando a questao1

--- atividade_1/src/App.js
+++ atividade_1/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
-import {Name, Course, City} from './components/Questao2';
+import Questao3 from './components/Questao3'
+import Questao3_1 from './components/Questao3_1'
 
 function App() {
   return (
     <div className="App">
-        <h2>
-          <Name />
-          <Course/> 
-          <City /> </h2>
+        <Questao3>
+          <Questao3_1 name="João Victor Falcão"/>
+          <Questao3_1 course="Eng. de Software"/>
+          <Questao3_1 city="Quixeramobim"/>
+        </Questao3>
     </div>
   );
 }
---
+++ atividade_1/src/components/Questao3_1.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => 
+    <div>
+        {props.name}
+        {props.course}
+        {props.city}
+    </div>
