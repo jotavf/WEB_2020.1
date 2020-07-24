74ae5e50a494f4468f46f1e419bff73d04131ef4
Joao Victor Falcao <jotavfalcao@gmail.com>
2020-07-23 21:18:38 -0300

    questao1

---
+++ atividade_1/src/components/Questao1.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+// function Id() {
+//     return(
+//       <div>
+//           <h1>João Victor Falcão</h1>
+//           <h1>Eng. de Software</h1>
+//           <h1>Quixeramobim</h1>
+//       </div>
+//     );
+// }
+
+// funções \/
+{
+function Name(){
+    return(
+        <h1>João Victor Falcão</h1>
+    );
+}
+
+function Course(){
+    return(
+        <h1>Eng. de Software</h1>
+    );
+}
+
+function City(){
+    return(
+        <h1>Quixeramobim</h1>
+    );
+}
+}
+
+function AllFunc() {
+    return(
+        <div>
+            <Name />
+            <Course />
+            <City />
+        </div>
+    );
+}
+
+export default AllFunc;
